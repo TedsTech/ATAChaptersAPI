07b4b26547e1ddbf8c746c31db08d2704f617511
18729 <damodarp1994@gmail.com>
2024-10-26 13:07:08 -0400

    all the chnages from dotenv -1

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,17 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerConfig from './swaggerConfig';
 import { Chapter } from './entity/Chapter';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'dpyakurel',
-  password: 'Trump',
-  database: 'ata_chapters',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [Chapter],
